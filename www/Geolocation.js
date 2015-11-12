b3b93b9610ac4b9fba88cd7ed544f04705d6e39d
Gen Liu <gg@linux-o4mm.site>
2015-11-12 19:36:03 +0800

    add js interface

--- www/Geolocation.js
+++ www/Geolocation.js
@@ -9,6 +9,8 @@ var ACTION_CLEAR_WATCH = 'clearWatch';
 
 var geolocation = module.exports = {};
 
+var idGenerator = 0;
+
 geolocation.getCurrentPosition = function (success, error, options) {
   cordova.exec(function(args) {
     success.apply(null, args);
@@ -16,9 +18,11 @@ geolocation.getCurrentPosition = function (success, error, options) {
 };
 
 geolocation.watchPosition = function (success, error, options) {
+  var watchId = idGenerator++;
   cordova.exec(function(args) {
     success.apply(null, args);
-  }, error, SERVICE_NAME, ACTION_WATCH_POSITION, [options]);
+  }, error, SERVICE_NAME, ACTION_WATCH_POSITION, [options, watchId]);
+  return watchId;
 };
 
 geolocation.clearWatch = function (watchId) {
